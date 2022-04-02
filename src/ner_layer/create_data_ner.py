5eb65e1f8a86bb36a4c2e775fad69f7c8fd48e1e
jose-lopez <josesmooth@gmail.com>
2022-04-02 16:28:43 -0400

    Optimizing the NER samples definition in create_data_ner.py

--- src/ner_layer/create_data_ner.py
+++ src/ner_layer/create_data_ner.py
@@ -261,11 +261,9 @@ def getting_ner_examples(files, sentences_number, corpus_length, percentage, mat
         print("randomizing.....")
         random.shuffle(SENTENCES)
 
-        global_proportion = sentences_number / corpus_length
+        sentences_to_tag = math.ceil(len(SENTENCES) * proportion)
 
-        sentences_to_tag = 2 * math.ceil(len(SENTENCES) * global_proportion)
-
-        if sentences_to_tag > 1000:
+        if sentences_to_tag >= 1000:
             SENTENCES = SENTENCES[:1000]
 
         print("tagging.....")
@@ -295,10 +293,10 @@ def getting_ner_examples(files, sentences_number, corpus_length, percentage, mat
 
 if __name__ == '__main__':
 
-    CORPUS_PATH = "data/corpus_en/"
+    CORPUS_PATH = "data/corpus/"
 
-    # PATTERNS_PATH = "data/patterns2.1.jsonl"
-    PATTERNS_PATH = "data/names_patterns_en.jsonl"
+    PATTERNS_PATH = "data/patterns2.1.jsonl"
+    # PATTERNS_PATH = "data/names_patterns_en.jsonl"
 
     sentences_number, percentage = get_arguments(sys.argv)
 
@@ -307,8 +305,8 @@ if __name__ == '__main__':
     print("\n" + "\n")
 
     print("Loading the model...")
-    # nlp = spacy.load("grc_ud_proiel_lg")
-    nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load("grc_ud_proiel_lg")
+    # nlp = spacy.load("en_core_web_sm")
     print(".. done" + "\n")
 
     print("Loading the entities' patterns...")
@@ -323,7 +321,7 @@ if __name__ == '__main__':
         CORPUS_PATH, sentences_number)
     # getting the required ner examples
     ner_examples = getting_ner_examples(
-        files, sentences_number, corpus_length, percentage, matcher)
+        files, sentences_number, corpus_length, percentage, proportion)
     print(".. done" + "\n")
 
     print(
